2617c6e75ecba145b6078953b1a1d0707e123f90
YuanchaoLi1 <2538656L@student.gla.ac.uk>
2021-07-26 19:51:57 +0100

    Chapter4 Exercise

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,7 +21,7 @@ print(os.path.dirname(os.path.dirname(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -71,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
+
             ],
         },
     },
@@ -130,3 +132,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
