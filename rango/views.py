6957b728ca333d5d1f1fad1e1ae056c73424f2fe
YuanchaoLi1 <2538656L@student.gla.ac.uk>
2021-07-27 10:15:11 +0100

    modify brunch chapter3

--- rango/views.py
+++ rango/views.py
@@ -7,16 +7,17 @@ from django.http import HttpResponse
 def index(request):
     # Ex4
     # Construct a dictionary to pass to the template engine as its context.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # return render(request, 'rango/index.html', context=context_dict)
 
     # Ex3
-    # return HttpResponse ("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>" )
+    return HttpResponse ("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>" )
 
 def about(request):  
     # Ex4
     # context_dictAbout = {'boldmessage': 'This tutorial has been put together by Yuanchao Li.'}
-    return render(request, 'rango/about.html')
+    # return render(request, 'rango/about.html')
+
     # Ex3
-    # return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>Index</a>")
+    return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>Index</a>")
     
