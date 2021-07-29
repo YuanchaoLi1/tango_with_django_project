1beec58b6cce0db649fd29342f1879e491bc2671
YuanchaoLi1 <2538656L@student.gla.ac.uk>
2021-07-29 14:07:33 +0100

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -15,6 +15,32 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+# Ex10
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None): 
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val 
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.set_cookie('last_visit', str(datetime.now()))
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+    # response.set_cookie('visits', visits)
+
+
 
 # Ex9
 def register(request):
@@ -139,6 +165,9 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 def index(request):
+    # Ex10
+    request.session.set_test_cookie()
+
     # Ex5/6
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -147,6 +176,12 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    # Ex10
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
     
     return render(request, 'rango/index.html', context_dict)
 
@@ -160,6 +195,17 @@ def index(request):
     return HttpResponse ("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>" )
 
 def about(request):  
+    #Ex10
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
+    
     # EX8
     print(request.method)
     print(request.user)
